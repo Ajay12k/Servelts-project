a599f5443e09add644fafafd01796a0e2cbf9b61
Ajay12k <ajaymarella0811@gmail.com>
2025-08-08 16:42:44 +0530

    Initial commit

---
+++ firstservlet.java
@@ -0,0 +1,20 @@
+// import jakarta.servlet.*;
+import java.io.*;
+ 
+import jakarta.servlet.http.*;
+public class firstservlet  extends HttpServlet
+{
+	 public  void service(HttpServletRequest request,HttpServletResponse response)
+	 {
+		try
+		{
+			 //object Print write
+		       PrintWriter out=response.getWriter();
+		     // what my server to respond
+		       out.println(" welcome to servelet example 1");
+		}
+		catch(Exception e)
+		{
+		}
+	 }
+}
